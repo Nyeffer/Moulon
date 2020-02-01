4dce9e3980e20da8541d2edb2b80e6a2c71492c5
Jeffherson Vega <jeffhersonlim@gmail.com>
2020-02-01 00:30:55 -0500

    Jump works

---
+++ Assets/Imported Package/True Explosions/System/Scripts/effects/exploderComponents/GroundCheck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private bool isGrounded = false;
+    void OnTriggerEnter(Collider col) {
+        if(col.gameObject.tag == "Ground") {
+            isGrounded = true;
+        }
+    }
+    
+    public bool GetGroundCheck() {
+        return isGrounded;
+    }
+}
---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public GroundCheck groundCheck;
+    public float jumpforce = 10.0f;
+    private bool isGrounded = true;
+    void Update() {
+        isGrounded = groundCheck.GetGroundCheck();
+        if(isGrounded) {
+                if(Input.GetKeyDown("space")) {
+                Debug.Log("Jump");
+                gameObject.GetComponent<Rigidbody>().AddForce(0.0f, jumpforce, 0.0f);
+            }
+        }
+    }
+}
