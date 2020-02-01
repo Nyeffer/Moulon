2d4f380bea75d7c4213b0caedee7ad0e5b66dd3d
Jeffherson Vega <jeffhersonlim@gmail.com>
2020-01-31 21:19:33 -0500

    Started the Bullet Behavior

---
+++ Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+
+    private GameObject target; // the game Object this object will seek
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTarget(GameObject myTarget) {
+        target = myTarget;
+    }
+}
