b74dae99dead9f709dfef9d02a5cf88ac619daf4
Jeffherson Vega <jeffhersonlim@gmail.com>
2020-01-31 20:47:56 -0500

    Fixed Movement

--- Assets/Scripts/RotatewithCam.cs
+++ Assets/Scripts/RotatewithCam.cs
@@ -22,28 +22,54 @@ public class RotatewithCam : MonoBehaviour
          transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
     }
 
-    void FixedUpdate()  {
-        //reading the input:
-        float horizontalAxis = Input.GetAxis("Horizontal");
-        float verticalAxis = Input.GetAxis("Vertical");
-         
-        //assuming we only using the single camera:
-        var camera = Camera.main;
- 
-        //camera forward and right vectors:
-        var forward = camera.transform.forward;
-        var right = camera.transform.right;
- 
-        //project forward and right vectors on the horizontal plane (y = 0)
-        forward.y = 0f;
-        right.y = 0f;
-        forward.Normalize();
-        right.Normalize();
- 
-        //this is the direction in the world space we want to move:
-        var desiredMoveDirection = forward * verticalAxis + right * horizontalAxis;
- 
-        //now we can apply the movement:
-        transform.Translate(desiredMoveDirection * MoveSpeed * Time.deltaTime);
-    }
+    void FixedUpdate(){
+            //assuming we're only using the single camera:
+                var camera = Camera.main;
+           
+                //Data recieved from CrossPlatformInput Handler:
+                  float horizontalAxis = Input.GetAxis("Horizontal");
+                  float verticalAxis = Input.GetAxis("Vertical");
+               
+            float facing = camera.transform.rotation.y;
+            float DistanceFromNeutral = 0;
+            float transformZ = 0;
+            float transformX = 0;
+            float finalZ = 0;
+            float finalX = 0;
+           
+            if (facing > -90 && facing <= 90){ //facing forward
+                if(facing >= 0){
+                    DistanceFromNeutral = (90 - facing);
+                }else{
+                    if(facing < 0){
+                        DistanceFromNeutral = (90 + facing);
+                    };
+                };
+               
+               
+                transformX = (1 / 90) * (DistanceFromNeutral);
+                transformZ = 90 - transformX;
+            };
+           
+            //for monitoring and debugging:
+            Debug.Log("Main Camera's <color=red>Y rotation</color> is <color=red>" + facing + "</color>, and the <color=blue>Distance from Neutral rotation</color> <color=purple>(0/90/180/270)</color> is <color=blue>" + DistanceFromNeutral + "</color>.");
+           
+            // continue
+           
+            finalX = (transformX * verticalAxis) + (transformZ * horizontalAxis);
+           
+            //if(finalX > 1){
+            //    finalX = 1;
+            //    };
+            // ^^^^ dont use, causes vector3 slowdowns or something ^^^^
+           
+            finalZ = (transformZ * verticalAxis) + (transformX * horizontalAxis);
+           
+            //if(finalZ > 1){
+            //    finalZ = 1;
+            //    };
+            // ^^^^ dont use, causes vector3 slowdowns or something ^^^^
+           
+            transform.Translate((new Vector3( finalX * 0.01f , 0f , finalZ * 0.01f ))* MoveSpeed * Time.deltaTime);
+}
 }
