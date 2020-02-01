d5b40a8cd7b570d14a2d21289356a541f4be41f3
Jeffherson Vega <jeffhersonlim@gmail.com>
2020-02-01 01:42:41 -0500

    Revise Game

---
+++ Assets/Scripts/ElevatorBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorBehavior : MonoBehaviour
+{
+    public GameObject destination;
+    public GameObject origin;
+    private bool isTravel = false;
+
+    void Update() {
+        if(isTravel) {
+            gameObject.transform.Translate(destination.transform.position,Space.World);
+        } else {
+            gameObject.transform.Translate(origin.transform.position,Space.World);
+        }
+    }
+    void OnTriggerEnter(Collider col) {
+        if(col.gameObject.tag == "Player") {
+            isTravel = true;
+            Debug.Log("StepOn");
+            col.gameObject.transform.parent = transform;
+        }
+    }
+
+    void OnTriggerExit(Collider col) {
+        if(col.gameObject.tag == "Player") {
+            isTravel = false;
+            Debug.Log("StepOff");
+			col.gameObject.transform.parent = null;
+        }
+    }
+}
