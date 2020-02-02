03dd6371bdd0741b6dd3ad6ab4a211f7c00373ad
Jeffherson Vega <jeffhersonlim@gmail.com>
2020-02-02 05:32:18 -0500

    Testing

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -7,22 +7,35 @@ public class EnemyManager : MonoBehaviour
 
     public GameObject[] Enemies;
     public float[] timeTillSpawn;
+    private int enemySpawned = 0;
+    private bool started = false;
 
     private float timeSince = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < Enemies.Length; i++) {
+            Enemies[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeSince += Time.deltaTime;
+        if(started) {
+            timeSince += Time.deltaTime;
+            if(timeTillSpawn[enemySpawned] >= timeSince) {
+                EnemyActive(enemySpawned);
+            }
+            if(enemySpawned >= Enemies.Length) {
+                started = false;
+            }
+        }
     }
 
     void EnemyActive(int EnemyNum) {
         Enemies[EnemyNum].SetActive(true);
+        enemySpawned += 1;
     }
 
 
--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -6,5 +6,14 @@ public class StateManager : MonoBehaviour
 {
     public GameObject messages;
     public EnemyManager enemyManager;
+    private bool isStarted = false; // the state before the enemy spawn timers starts
+
+    void Update() {
+
+    }
+
+    public void StartNow() {
+        isStarted = true;
+    }
 
 }
