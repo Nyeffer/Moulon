2ea89efd7c53642cc1b8ce5a2ec79248c3bbee55
Jeffherson Vega <jeffhersonlim@gmail.com>
2020-02-02 03:12:40 -0500

    Made Enemy Manager

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    public Transform[] Waypoints;
+    public GameObject[] Paths;
+
+    public GameObject[] Enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
